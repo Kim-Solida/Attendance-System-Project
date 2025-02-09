726b1f2c12e3f96ef09e4a23ad9c2141eedaadf3
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-02-09 20:17:33 +0700

    Admin.java

--- Admin.java
+++ Admin.java
@@ -1,18 +1,18 @@
+
 public class Admin {
-    
-    private String adminId;      
-    private String username;     
-    private String password;    
-    private String fullName;     
-    private String email;        
-    private String phoneNumber;  
-    private String role;         
-    private String createdAt;    
-    private boolean isActive;    
+
+    private String adminId;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String role;
+    private String createdAt;
+    private boolean isActive;
 
     // Constructor
-    public Admin(String adminId, String username, String password, String fullName,  String email, String phoneNumber, String role, String createdAt, boolean isActive) 
-    {
+    public Admin(String adminId, String username, String password, String fullName, String email, String phoneNumber, String role, String createdAt, boolean isActive) {
         this.adminId = adminId;
         this.username = username;
         this.password = password;
@@ -23,4 +23,78 @@ public class Admin {
         this.createdAt = createdAt;
         this.isActive = isActive;
     }
+
+    // Getters and Setters
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
 }
