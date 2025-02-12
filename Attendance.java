8bef5b939456fe143581c8ba05e0d447e90d038a
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-02-12 20:29:13 +0700

    Attendance.java

--- Attendance.java
+++ Attendance.java
@@ -1,48 +1,114 @@
 
-public class Attendance {
+public class Admin {
 
-    private String attendanceId;
-    private String studentId;
-    private String classDate;
-    private String status;
+    private String adminId;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String role;
+    private String createdAt;
+    private boolean isActive;
 
-    public Attendance(String attendanceId, String studentId, String classDate, String status) {
-        this.attendanceId = attendanceId;
-        this.studentId = studentId;
-        this.classDate = classDate;
-        this.status = status;
+    // Constructor
+    public Admin(String adminId, String username, String password, String fullName, String email, String phoneNumber, String role, String createdAt, boolean isActive) {
+        this.adminId = adminId;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.isActive = isActive;
     }
 
     // Getters and Setters
-    public String getAttendanceId() {
-        return attendanceId;
+    public String getAdminId() {
+        return adminId;
     }
 
-    public void setAttendanceId(String attendanceId) {
-        this.attendanceId = attendanceId;
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
     }
 
-    public String getStudentId() {
-        return studentId;
+    public String getUsername() {
+        return username;
     }
 
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getClassDate() {
-        return classDate;
+    public String getPassword() {
+        return password;
     }
 
-    public void setClassDate(String classDate) {
-        this.classDate = classDate;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getStatus() {
-        return status;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    //isEqual Method
+    public boolean isEqual(Admin otheraAdmin) {
+        return this.adminId.equals(otheraAdmin.adminId);
+    }
+
+    //toString Method
+    @Override
+    public String toString() {
+        return "Admin ID: " + adminId
+                + ", Username: " + username
+                + ", Full Name: " + fullName
+                + ", Role: " + role;
+    }
+
 }
