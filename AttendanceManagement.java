1a298ebd1e04b26ababdb985179b6b866819967f
Sreypich183 <lyysreypich2@gmail.com>
2025-02-12 15:27:48 +0700

    Create AttendanceManagement.java

---
+++ AttendanceManagement.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class AttendanceManagement {
+    private ArrayList<Student> students;
+
+    public AttendanceManagement() {
+        students = new ArrayList<>();
+    }
+}
