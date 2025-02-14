2805b93de2b53ac9f3a5e7d8caf741636d821da0
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-02-14 21:30:09 +0700

    Admin.java

--- Admin.java
+++ Admin.java
@@ -1,46 +1,14 @@
 
-public class Admin implements Authentication {
+public class Admin extends User {
 
     private String adminId;
-    private String adminFirstName;
-    private String adminLastName;
-    private String adminPassword;
-    private String adminAge;
-    private String sex;
-    private String email;
-    private String phoneNumber;
     private String role;
 
     // Constructor
-    public Admin(String adminId, String adminFirstName, String adminLastName, String adminPassword, String adminAge, String sex, String email, String phoneNumber, String role) {
+    public Admin(String firstName, String lastName, String password, String age, String sex, String email, String phoneNumber, String adminId, String role) {
+        super(firstName, lastName, password, age, sex, email, phoneNumber); // Call the superclass constructor
         this.adminId = adminId;
-        this.adminFirstName = adminFirstName;
-        this.adminLastName = adminLastName;
-        this.adminPassword = adminPassword;
-        this.adminAge = adminAge;
-        this.sex = sex;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
         this.role = role;
-    }
-
-    @Override
-    public void register(String adminFirstName, String adminLastName, String adminPassword) {
-        this.adminFirstName = adminFirstName;
-        this.adminLastName = adminLastName;
-        this.adminPassword = adminPassword;
-        System.out.println("Admin registered successfully!");
-    }
-
-    // Implementing login method from Authentication interface
-    public void login(String adminFirstName, String adminLastName, String adminPassword) {
-        if (this.adminFirstName.equals(adminFirstName)
-                && this.adminLastName.equals(adminLastName)
-                && this.adminPassword.equals(adminPassword)) {
-            System.out.println("Login successful! Welcome, " + this.adminFirstName + " " + this.adminLastName);
-        } else {
-            System.out.println("Login failed! Incorrect username or password.");
-        }
     }
 
     // Getters and Setters
@@ -52,66 +20,6 @@ public class Admin implements Authentication {
         this.adminId = adminId;
     }
 
-    public String getAdminFirstName() {
-        return adminFirstName;
-    }
-
-    public void setAdminFirstName(String adminFirstName) {
-        this.adminFirstName = adminFirstName;
-    }
-
-    public String getAdminLastName() {
-        return adminLastName;
-    }
-
-    public void setAdminLastName(String adminLastName) {
-        this.adminLastName = adminLastName;
-    }
-
-    public String getFullName() {
-        return adminFirstName + " " + adminLastName;
-    }
-
-    public String getAdminPassword() {
-        return adminPassword;
-    }
-
-    public void setAdminPassword(String password) {
-        this.adminPassword = password;
-    }
-
-    public String getAdminAge() {
-        return adminAge;
-    }
-
-    public void setAdminAge(String adminAge) {
-        this.adminAge = adminAge;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public String getRole() {
         return role;
     }
@@ -121,15 +29,15 @@ public class Admin implements Authentication {
     }
 
     //isEqual Method
-    public boolean isEqual(Admin otheraAdmin) {
-        return this.adminId.equals(otheraAdmin.adminId);
+    public boolean isEqual(Admin otherAdmin) {
+        return this.adminId.equals(otherAdmin.adminId);
     }
 
     //toString Method
     @Override
     public String toString() {
         return "Admin ID: " + adminId
-                + ", Admin First Name: " + adminFirstName + " " + adminLastName
+                + ", Name: " + getFirstName() + " " + getLastName()
                 + ", Role: " + role;
     }
 
