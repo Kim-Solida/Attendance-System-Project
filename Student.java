e99b9c764b4a9666bf80981dd0cb4743d9f72d9b
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-02-14 21:30:40 +0700

    Student.java

--- Student.java
+++ Student.java
@@ -1,51 +1,18 @@
 
-public class Student implements Authentication {
+public class Student extends User {
 
     private String studentId;
-    private String studentFirstName;
-    private String studentLastName;
-    private String studentPassword;
     private String dob;
-    private String studentAge;
-    private String sex;
     private String address;
-    private String email;
-    private String phoneNumber;
     private String enrollmentDate;
 
     // Constructor
-    public Student(String studentId, String studentFirstName, String studentLastName, String dob,
-            String studentAge, String sex, String address, String email,
-            String phoneNumber, String enrollmentDate) {
+    public Student(String firstName, String lastName, String password, String age, String sex, String email, String phoneNumber, String studentId, String dob, String address, String enrollmentDate) {
+        super(firstName, lastName, password, age, sex, email, phoneNumber);
         this.studentId = studentId;
-        this.studentFirstName = studentFirstName;
-        this.studentLastName = studentLastName;
         this.dob = dob;
-        this.studentAge = studentAge;
-        this.sex = sex;
         this.address = address;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
         this.enrollmentDate = enrollmentDate;
-    }
-
-    @Override
-    public void register(String studentFirstName, String studentLastName, String studentPassword) {
-        this.studentFirstName = studentFirstName;
-        this.studentLastName = studentLastName;
-        this.studentPassword = studentPassword;
-        System.out.println("Student registered successfully!");
-    }
-
-    @Override
-    public void login(String studentFirstName, String studentLastName, String studentPassword) {
-        if (this.studentFirstName.equals(studentFirstName)
-                && this.studentLastName.equals(studentLastName)
-                && this.studentPassword.equals(studentPassword)) {
-            System.out.println("Login successful! Welcome, " + this.studentFirstName + " " + this.studentLastName);
-        } else {
-            System.out.println("Login failed! Incorrect username or password.");
-        }
     }
 
     // Getters and Setters
@@ -57,26 +24,6 @@ public class Student implements Authentication {
         this.studentId = studentId;
     }
 
-    public String getStudentFirstName() {
-        return studentFirstName;
-    }
-
-    public void setStudentFirstName(String studentFirstName) {
-        this.studentFirstName = studentFirstName;
-    }
-
-    public String getStudentLastName() {
-        return studentLastName;
-    }
-
-    public void setStudentLastName(String studentLastName) {
-        this.studentLastName = studentLastName;
-    }
-
-    public String getFullName() {
-        return studentFirstName + " " + studentLastName;
-    }
-
     public String getDob() {
         return dob;
     }
@@ -85,44 +32,12 @@ public class Student implements Authentication {
         this.dob = dob;
     }
 
-    public String getStudentAge() {
-        return studentAge;
-    }
-
-    public void setStudentAge(String studentAge) {
-        this.studentAge = studentAge;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public String getEnrollmentDate() {
@@ -142,10 +57,10 @@ public class Student implements Authentication {
     @Override
     public String toString() {
         return "Student ID: " + studentId
-                + ", Name: " + studentFirstName + " " + studentLastName
+                + ", Name: " + getFirstName() + " " + getLastName()
                 + ", DOB: " + dob
-                + ", Age: " + studentAge
-                + ", Sex: " + sex
+                + ", Age: " + getAge()
+                + ", Sex: " + getSex()
                 + ", Enrollment Date: " + enrollmentDate;
     }
 }
