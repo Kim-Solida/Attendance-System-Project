9d685415a8161ad45a4eb8545deb1aff577115f6
Sreypich183 <lyysreypich2@gmail.com>
2025-03-04 10:43:09 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,14 +1,23 @@
-
 public class Admin extends User {
 
     private String adminId;
     private String role;
+    private String department;
+    private String employeeId;
+    private String dateOfBirth;
+    private String workAddress;
+    private String hireDate;
 
     // Constructor
-    public Admin(String firstName, String lastName, String password, String age, String sex, String email, String phoneNumber, String adminId, String role) {
+    public Admin(String firstName, String lastName, String password, String age, String sex, String email, String phoneNumber, String adminId, String role, String department, String employeeId, String dateOfBirth, String workAddress, String hireDate) {
         super(firstName, lastName, password, age, sex, email, phoneNumber); // Call the superclass constructor
         this.adminId = adminId;
         this.role = role;
+        this.department = department;
+        this.employeeId = employeeId;
+        this.dateOfBirth = dateOfBirth;
+        this.workAddress = workAddress;
+        this.hireDate = hireDate;
     }
 
     // Getters and Setters
@@ -27,18 +36,175 @@ public class Admin extends User {
     public void setRole(String role) {
         this.role = role;
     }
-
-    //isEqual Method
-    public boolean isEqual(Admin otherAdmin) {
-        return this.adminId.equals(otherAdmin.adminId);
+    
+    public String getDepartment() {
+        return department;
     }
 
-    //toString Method
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(String workAddress) {
+        this.workAddress = workAddress;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    // Equals Method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Admin otherAdmin = (Admin) obj;
+        return adminId.equals(otherAdmin.adminId);
+    }
+
+    // toString Method
     @Override
     public String toString() {
         return "Admin ID: " + adminId
                 + ", Name: " + getFirstName() + " " + getLastName()
-                + ", Role: " + role;
+                + ", Role: " + role
+                + ", Department: " + department
+                + ", Employee ID: " + employeeId
+                + ", DOB: " + dateOfBirth
+                + ", Work Address: " + workAddress
+                + ", Hire Date: " + hireDate;
+    }
+}
+public class Admin extends User {
+
+    private String adminId;
+    private String role;
+    private String department;
+    private String employeeId;
+    private String dateOfBirth;
+    private String workAddress;
+    private String hireDate;
+
+    // Constructor
+    public Admin(String firstName, String lastName, String password, String age, String sex, String email, String phoneNumber, String adminId, String role, String department, String employeeId, String dateOfBirth, String workAddress, String hireDate) {
+        super(firstName, lastName, password, age, sex, email, phoneNumber); // Call the superclass constructor
+        this.adminId = adminId;
+        this.role = role;
+        this.department = department;
+        this.employeeId = employeeId;
+        this.dateOfBirth = dateOfBirth;
+        this.workAddress = workAddress;
+        this.hireDate = hireDate;
     }
 
+    // Getters and Setters
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(String workAddress) {
+        this.workAddress = workAddress;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    // Equals Method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Admin otherAdmin = (Admin) obj;
+        return adminId.equals(otherAdmin.adminId);
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Admin ID: " + adminId
+                + ", Name: " + getFirstName() + " " + getLastName()
+                + ", Role: " + role
+                + ", Department: " + department
+                + ", Employee ID: " + employeeId
+                + ", DOB: " + dateOfBirth
+                + ", Work Address: " + workAddress
+                + ", Hire Date: " + hireDate;
+    }
 }
