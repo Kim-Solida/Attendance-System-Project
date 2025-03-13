ede5f7278c0d2fbacfe3e8ca0ed77f0a17748b83
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-03-13 21:34:17 +0700

    Create UserNotFoundException.java

---
+++ UserNotFoundException.java
@@ -0,0 +1,11 @@
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
